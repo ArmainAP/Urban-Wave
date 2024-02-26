b3264884608ceaaf7d94a7a719a639478f9ceaee
ArmainAP <adrian.popoviciu@katcodelabs.com>
2024-02-26 15:11:51 +0200

    +Added new project files

---
+++ Source/UrbanWave/UrbanWaveGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UrbanWaveGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class URBANWAVE_API AUrbanWaveGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
